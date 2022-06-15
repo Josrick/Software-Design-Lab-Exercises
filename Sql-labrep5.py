d297862d9f5f67987bda08f2544c38af84032b25
Jombie <98839243+Josrick@users.noreply.github.com>
2022-06-16 02:22:08 +0800

    Add files via upload
    Python SQLite Database Connection

---
+++ Sql-labrep5.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+try:
+    sqliteConnection = sqlite3.connect('sql.db')
+    cursor = sqliteConnection.cursor()
+    print('DB Init')
+
+
+    query = 'select sqlite_version();'
+    cursor.execute(query)
+
+
+    result = cursor.fetchall()
+    print('SQLite Version is {}'.format(result))
+
+
+    cursor.close()
+
+
+except sqlite3.Error as error:
+    print('Error occured - ', error)
+
+
+finally:
+
+    if sqliteConnection:
+        sqliteConnection.close()
+        print('SQLite Connection closed')
